b2cc4563f407766a1cb117873e58b4589220d6c9
Nilton da Silva Barros <barros.nilton@yahoo.com.br>
2026-02-01 11:57:06 -0300

    Primeiro Projeto em Python

---
+++ aula1-ex1.py
@@ -0,0 +1,15 @@
+
+print ("Boa tarde!")
+
+valor1=10
+valor2=15
+
+soma=valor1+valor2
+subtracao=valor1-valor2
+mutiplicacao=valor1*valor2
+divisao=valor1/valor2
+
+print("O resultado da soma é: ",soma)
+print("O resultado da subtração é: ",subtracao)
+print("O resultado da mutiplicação é: ",mutiplicacao)
+print("O resultado da divisão é: ",divisao)
